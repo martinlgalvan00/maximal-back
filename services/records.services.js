b4a489590ea54a8287ce24419c390bc6e74c5dec
martinlgalva <martinlgalvan@yahoo.com>
2023-08-20 17:41:50 -0300

    host new

--- services/records.services.js
+++ services/records.services.js
@@ -1,7 +1,7 @@
 import { MongoClient, ObjectId } from 'mongodb'
 
 const client = new MongoClient('mongodb://m4rt1n:S0YM4RT1N@62.72.51.41:27017/')
-const db = client.db('MAXIMAL_STRCORP')
+const db = client.db('TOM') //LE ERRÉ A LA HORA DE PONER EL NOMBRE, POR LO QUE EN VEZ DE MAXIMAL_SRTCORP ES TOM
 const records = db.collection('Records')
 
 async function getSquatRecords(clase, category, sex){
