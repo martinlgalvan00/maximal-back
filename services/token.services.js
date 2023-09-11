279573d113fd064dbe0e990156931f53b7f14555
martinlgalvan00 <122166238+martinlgalvan00@users.noreply.github.com>
2023-09-11 17:17:22 -0300

    Update token.services.js

--- services/token.services.js
+++ services/token.services.js
@@ -1,6 +1,6 @@
 import { MongoClient, ObjectId } from 'mongodb'
 
-const client = new MongoClient('mongodb://m4rt1n:S0YM4RT1N@62.72.51.41:27017/')
+const client = new MongoClient('mongodb://m4rt1n:s0yM4RT1NG4LV4N@62.72.51.41:27017/')
 const db = client.db('MAXIMAL_STRCORP')
 const tokens = db.collection('Tokens')
 
