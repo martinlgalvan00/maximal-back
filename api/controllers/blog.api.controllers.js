6505f4f30c2b74c6b686ec1fb58e557d05646234
martinlgalva <108037810+martinlgalvan@users.noreply.github.com>
2024-08-11 16:01:29 -0300

    images

--- api/controllers/blog.api.controllers.js
+++ api/controllers/blog.api.controllers.js
@@ -1,93 +1,91 @@
+import * as BlogService from '../../services/blog.services.js';
+import { upload } from '../middleware/upload.middleware.js';
 
-import * as BlogService from '../../services/blog.services.js'
-
-import { upload } from '../middleware/upload.middleware.js'
-
-function findAllBlogs(req, res){
-
+function findAllBlogs(req, res) {
     BlogService.getAllBlogs()
-        .then(function(blog){
-            res.status(200).json(blog)
-        })
+        .then(function (blogs) {
+            res.status(200).json(blogs);
+        });
 }
 
-function findByBlogId(req, res){
-    const id = req.params.id_blog
+function findByBlogId(req, res) {
+    const id = req.params.id_blog;
 
     BlogService.getBlogById(id)
-        .then(function(blog){
-            if(blog){
-                res.status(200).json(blog)
-            } else{
-                res.status(404).json({message: "Blog no encontrado."})
+        .then(function (blog) {
+            if (blog) {
+                res.status(200).json(blog);
+            } else {
+                res.status(404).json({ message: "Blog no encontrado." });
             }
-        })
-       
+        });
 }
 
-function createBlog(req, res){
+function createBlog(req, res) {
+    let blog;
 
-    const blog = {}
-
-    if(req.body.name){
-        blog.name = req.body.name
+    // Extraer y parsear los datos JSON enviados en el campo 'data' del FormData
+    try {
+        blog = JSON.parse(req.body.data);
+    } catch (error) {
+        return res.status(400).json({ message: 'Datos del blog mal formateados.' });
     }
 
-    if(req.body.info){
-        blog.info = req.body.info
+    // Manejar la imagen si se envía
+    if (req.file) {
+        blog.image = req.file.filename;  // Suponiendo que uses multer para manejar las imágenes
     }
-
 
     BlogService.createBlog(blog)
-        .then(function(blog){
-            if(blog){
-                res.status(200).json(blog)
-            } else{
-                res.status(404).json({message: "Blog no creado."})
+        .then(function (createdBlog) {
+            if (createdBlog) {
+                res.status(200).json(createdBlog);
+            } else {
+                res.status(500).json({ message: "Blog no creado." });
             }
-        })
+        });
+}
+
+function editBlog(req, res) {
+    const id = req.params.id_blog;
+    let blog;
+
+    // Extraer y parsear los datos JSON enviados en el campo 'data' del FormData
+    try {
+        blog = JSON.parse(req.body.data);
+    } catch (error) {
+        return res.status(400).json({ message: 'Datos del blog mal formateados.' });
     }
 
-function editBlog(req, res){
-    const id = req.params.id_blog
-
-    const blog = {}
-
-    if(req.body.name){
-        blog.name = req.body.name
-    } 
-
-    if(req.body.info){
-        blog.info = req.body.info
-    } 
-
+    // Manejar la imagen si se envía
+    if (req.file) {
+        blog.image = req.file.filename;  // Suponiendo que uses multer para manejar las imágenes
+    }
 
     BlogService.editBlog(id, blog)
-        .then(function(){
-            return BlogService.getBlogById(id)
+        .then(function () {
+            return BlogService.getBlogById(id);
         })
-        .then(function(blog) {
-            if(blog){
-                res.status(200).json({message: "Blog modificada con éxito."})
+        .then(function (updatedBlog) {
+            if (updatedBlog) {
+                res.status(200).json({ message: "Blog modificado con éxito." });
             } else {
-                res.status(404).json({ message: "Blog no encontrado."})
+                res.status(404).json({ message: "Blog no encontrado." });
             }
-        })
-
+        });
 }
 
 function deleteBlog(req, res) {
-    const id = req.params.id_blog
+    const id = req.params.id_blog;
 
     BlogService.deleteBlog(id)
         .then(() => {
-            res.json({ message: 'Blog eliminado' })
+            res.json({ message: 'Blog eliminado' });
         })
         .catch(err => {
-            res.status(500).json({ message: err.message })
-        })
+            res.status(500).json({ message: err.message });
+        });
 }
-
 
 export {
     findAllBlogs,
@@ -95,4 +93,4 @@ export {
     createBlog,
     editBlog,
     deleteBlog,
-}
+};
